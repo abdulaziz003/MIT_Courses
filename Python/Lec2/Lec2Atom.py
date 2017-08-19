90b9ae74daada7c980ba6756329abc0b2d699eb9
AzoozMac <ABDULAZIZ@abdulazizs-MacBook-Pro.local>
2017-08-19 16:56:33 +0300

    new Atom version

---
+++ Python/Lec2/Lec2Atom.py
@@ -0,0 +1,10 @@
+#sample code for use in Lecture 2, Fall 2008 created by Abdulaziz
+
+y= 0
+x = -3
+itersLeft = x
+while (itersLeft>0):
+	y = y+x
+	itersLeft = itersLeft - 1
+	#print 'y = ', y. ', itersLeft = ', itersLeft
+print 'y is : ',y
